8f8c60cff7ad52ad656f6142baddc2fcb7eef4fe
tasiov <tasiovictoria@gmail.com>
2016-02-05 15:32:26 -0800

    adds messages table to schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,23 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE Users (
+  id int PRIMARY KEY, 
+  username VARCHAR(40) 
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE Rooms (
+  id int PRIMARY KEY, 
+  roomname VARCHAR(40)
+);
 
-
+CREATE TABLE Messages (
+  id int PRIMARY KEY, 
+  date Date NOT NULL, 
+  text text,
+  FOREIGN KEY (id) REFERENCES Users(id),
+  FOREIGN KEY (id) REFERENCES Rooms(id)
+);
 
 
 /*  Execute this file from the command line by typing:
