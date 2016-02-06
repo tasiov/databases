9835e079a00bfef970c78920190c4c338a6bc829
tasiov <tasiovictoria@gmail.com>
2016-02-05 16:01:04 -0800

    add db helper functions

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,16 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var dbConnection = mysql.createConnection({
+    user: 'root',
+    password: '',
+    database: 'chat'
+  });
 
+exports.connect = function() {
+  dbConnection.connect();
+};
+  
+exports.query = function(queryStr) {
+  dbConnection.query(queryStr);
+};
